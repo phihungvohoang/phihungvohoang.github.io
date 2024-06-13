c0c4c99bc3f659d3879f18da40f87cf8b1d52901
Hazel <phihungvohoang@gmail.com>
2024-06-13 11:55:13 +0700

    update script, change icon

--- js/script.js
+++ js/script.js
@@ -54,20 +54,18 @@ $(document).ready(function () {
         .removeClass("active");
     }
   });
-  $.getJSON(
-    "http://113.161.161.246:9001/_/hazel/database.json",
-    function (data) {
-      const categories = data.categories;
-      categories.forEach((category) => {
-        const categoryElement = $(`#${category.id}`);
-        category.subcategories.forEach((subcategory) => {
-          let productHtml = `
+  $.getJSON("113.161.161.246:9001/_/hazel/database.json", function (data) {
+    const categories = data.categories;
+    categories.forEach((category) => {
+      const categoryElement = $(`#${category.id}`);
+      category.subcategories.forEach((subcategory) => {
+        let productHtml = `
           <div class="listPage__item--title">
               <p class="title__text">${subcategory.name}</p>
           </div>    
           <div class="listPage__item--listProduct">`;
-          subcategory.products.forEach((product) => {
-            productHtml += `
+        subcategory.products.forEach((product) => {
+          productHtml += `
                 <div class="listProduct__item">
                     <div class="item__wrapImg">
                         <img class="item__wrapImg--img" src="${product.image}" />
@@ -83,14 +81,13 @@ $(document).ready(function () {
                     </div>
                 </div>
             `;
-          });
-          productHtml += `</div>`;
-
-          categoryElement.append(productHtml);
         });
+        productHtml += `</div>`;
+
+        categoryElement.append(productHtml);
       });
-    }
-  );
+    });
+  });
 });
 
 function selectTab(event, tabName) {
